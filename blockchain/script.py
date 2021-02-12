2f26ac8cfb452abc7143cb0cea07a68cf8ba46f2
westcallista <77295181+westcallista@users.noreply.github.com>
2021-02-12 09:45:41 -0800

    Create script.py

---
+++ blockchain/script.py
@@ -0,0 +1,18 @@
+from blockchain import Blockchain
+  
+block_one_transactions = {"sender":"Alice", "receiver": "US Government", "vote":"red"}
+block_two_transactions = {"sender": "Bob", "receiver":"US Government", "vote":"blue"}
+block_three_transactions = {"sender":"Nate", "receiver":"US Government", "vote":"red"}
+fake_transactions = {"sender": "Joe", "receiver":"US Government", "vote":"blue"}
+
+
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+local_blockchain.chain[2].transactions = fake_transactions
+local_blockchain.validate_chain()
+
