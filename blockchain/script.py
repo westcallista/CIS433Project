9d67e16010b077852f780928c7360ad3440a2573
westcallista <77295181+westcallista@users.noreply.github.com>
2021-02-22 15:20:03 -0800

    Update script.py

--- blockchain/script.py
+++ blockchain/script.py
@@ -1,14 +1,14 @@
 from blockchain import Blockchain
   
-block_one_transactions = {"sender":"Alice", "receiver": "US Government", "vote":"red"}
+block_one_transactions = {"sender":"Alice", "receiver": "US Government", "vote": "blue"}
 block_two_transactions = {"sender": "Bob", "receiver":"US Government", "vote":"blue"}
 block_three_transactions = {"sender":"Nate", "receiver":"US Government", "vote":"red"}
 fake_transactions = {"sender": "Joe", "receiver":"US Government", "vote":"blue"}
 
-
+print("beginning of script\n")
 local_blockchain = Blockchain()
 local_blockchain.print_blocks()
-
+print("after blockchain...\n")
 local_blockchain.add_block(block_one_transactions)
 local_blockchain.add_block(block_two_transactions)
 local_blockchain.add_block(block_three_transactions)
