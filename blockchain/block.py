57a46ef01faf70adea3af252f91d7f5a7d5ce2f4
westcallista <77295181+westcallista@users.noreply.github.com>
2021-02-22 15:17:37 -0800

    Update block.py

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,11 +1,16 @@
 import datetime
 from hashlib import sha256
 
+red = 0
+blue = 0
+
 class Block:
   def __init__(self, transactions, previous_hash):
     self.time_stamp = datetime.datetime.now()
     self.transactions = transactions
     self.previous_hash = previous_hash
+    #self.red = 0
+    #self.blue = 0
     self.nonce = 0
     self.hash = self.generate_hash()
 
@@ -15,7 +20,27 @@ class Block:
     return block_hash.hexdigest()
 
   def print_contents(self):
+    global red
+    global blue
     print("timestamp:", self.time_stamp)
     print("transactions:", self.transactions)
     print("current hash:", self.generate_hash())
     print("previous hash:", self.previous_hash)
+    if(self.transactions != []):
+      print("vote color:", self.transactions["vote"])
+      if(self.transactions["vote"] == "red"):
+        red += 1
+      else:
+        blue +=1
+    print("\n--------The Democrat candidate has " + str(blue) + " votes and the Republican candidate has " + str(red) + " votes.--------\n")
+
+
+  """
+  def vote_count(self):
+    if(self.transactions != []):
+      print("vote color:", self.transactions["vote"])
+      if(self.transactions["vote"] == "red"):
+        self.red += 1
+      else:
+        self.blue +=1
+  """
